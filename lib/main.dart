134e40541fa14cf3885a551a141415978694f36c
boring-km <kms0644804@naver.com>
2021-12-12 22:43:45 +0900

    [ST-04] dotenv 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:simpletodo/controller/login_controller.dart';
 import 'package:simpletodo/controller/main_controller.dart';
 import 'package:simpletodo/firebase_options.dart';
 import 'package:simpletodo/view/login_page.dart';
 import 'package:simpletodo/view/main_page.dart';
-// import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
+  await dotenv.load(fileName: 'dev.env');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/provider/main_provider.dart
+++ lib/provider/main_provider.dart
@@ -6,10 +6,11 @@ import 'package:simpletodo/domain/schedule_res.dart';
 
 class MainProvider extends GetConnect {
 
-  final url = 'http://192.168.200.166:8080/api';
+  String url = 'http://192.168.200.166:8080/api'; // Test URL
 
   Future<List<ScheduleRes>> callData(String token) async {
-    var header = withToken(token);
+    // url = dotenv.env['API_URL']!;
+    var header = _withToken(token);
     final response = await get('$url/schedule', headers: header,);
     final String? body = response.bodyString;
     if (body == null || body.isEmpty) {
@@ -23,7 +24,7 @@ class MainProvider extends GetConnect {
     return result;
   }
 
-  Map<String, String> withToken(String token) {
+  Map<String, String> _withToken(String token) {
     Map<String, String> header = HashMap();
     header['Authorization'] = 'Bearer $token';
     return header;
