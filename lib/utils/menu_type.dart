75bf66844965ca8d1f72d62b6fe8949bd92b537d
boring-km <kms0644804@naver.com>
2021-12-17 23:51:30 +0900

    [ST-04] popup UI 추가

---
+++ lib/utils/menu_type.dart
@@ -0,0 +1,5 @@
+enum MenuType {
+  update,
+  detail,
+  delete
+}
--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simpletodo/controller/main_controller.dart';
 import 'package:simpletodo/domain/schedule_res.dart';
 import 'package:simpletodo/utils/Log.dart';
+import 'package:simpletodo/utils/menu_type.dart';
 
 class MainPage extends GetView<MainController> {
   final _inputController = TextEditingController();
 
   final _scrollController = ScrollController();
+
+  final _popupTextController = TextEditingController();
 
   MainPage({Key? key}): super(key: key) {
     controller.getTodoList();
@@ -103,17 +107,54 @@ class MainPage extends GetView<MainController> {
                 controller.changeDoneYn(i);
                 FocusScope.of(context).unfocus();
               },
+              onLongPress: () {
+
+              },
               child: Row(
                 mainAxisSize: MainAxisSize.max,
                 children: [
                   _buildCheckImage(todoList[i].doneYn),
-                  const SizedBox(width: 8,),
+                  widthMargin(8),
                   Expanded(
                     child: Text(todoList[i].title!,
                       style: const TextStyle(
                         fontSize: 24,
                       ),
                     ),
+                  ),
+                  widthMargin(8),
+                  PopupMenuButton(
+                    child: Image.asset('images/ic_more_vert.png'),
+                    offset: Offset(0, 24),
+                    itemBuilder: (context) {
+                      return [
+                        const PopupMenuItem(
+                          child: Text('수정하기'),
+                          value: MenuType.update,
+                        ),
+                        const PopupMenuItem(
+                          child: Text('상세내용'),
+                          value: MenuType.detail,
+                        ),
+                        const PopupMenuItem(
+                          child: Text('삭제하기'),
+                          value: MenuType.delete,
+                        ),
+                      ];
+                    },
+                    onSelected: (menu) {
+                      switch(menu) {
+                        case MenuType.update:
+                          showUpdateDialog(context, todoList[i].title ?? '');
+                          break;
+                        case MenuType.detail:
+                          showDetailDialog(context, todoList[i]);
+                          break;
+                        case MenuType.delete:
+                          showDeleteDialog(context);
+                          break;
+                      }
+                    },
                   ),
                 ],
               ),
@@ -123,6 +164,113 @@ class MainPage extends GetView<MainController> {
       ),
     );
   }
+
+  void showDeleteDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text('삭제하기'),
+          content: Text('정말로 삭제하시겠습니까?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('취소',
+                style: TextStyle(color: Colors.grey,),
+              ),
+              onPressed: () {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              child: Text('삭제'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showDetailDialog(BuildContext context, ScheduleRes item) {
+    String title = item.title ?? '';
+    String des = item.des ?? '';
+    _popupTextController.text = des;
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text(title),
+          content: TextField(
+            controller: _popupTextController,
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              border: const UnderlineInputBorder(),
+              suffixIcon: IconButton(
+                onPressed: _popupTextController.clear,
+                icon: const Icon(CupertinoIcons.clear_thick_circled),
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('취소',
+                style: TextStyle(color: Colors.grey,),
+              ),
+              onPressed: () {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('수정'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showUpdateDialog(BuildContext context, String before) {
+    _popupTextController.text = before;
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text('수정하기'),
+          content: TextField(
+            controller: _popupTextController,
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              border: const UnderlineInputBorder(),
+              suffixIcon: IconButton(
+                onPressed: _popupTextController.clear,
+                icon: const Icon(CupertinoIcons.clear_thick_circled),
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('취소',
+                style: TextStyle(color: Colors.grey,),
+              ),
+              onPressed: () {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('수정'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  SizedBox widthMargin(double width) => SizedBox(width: width,);
 
   Image _buildCheckImage(bool? doneYn) {
     if (doneYn == null || !doneYn) {
